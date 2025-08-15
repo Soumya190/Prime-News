bca90ec08af9dc414a889a8a3d1e3d8a2a18018b
soumya tiwari <soumyatiwari7866@gmail.com>
2025-08-15 06:23:10 +0530

    Modified header file

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
 
 ## Getting Started
@@ -35,7 +34,6 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
-=======
 # Prime-News
 A responsive news platform with category-based filtering, keyword search, and pagination, built using Next and SASS.
 
@@ -71,4 +69,3 @@ src
  ┣ Components
  ┣ app
  ┣ Styles
->>>>>>> 51b918afbc09502dcd41c2a42293a57c7f472e82
--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -4,16 +4,27 @@ import styles from "@/Styles/Header/header.module.scss";
 export const Header = () => {
   return (
     <div className={styles.mainContainer}>
-        <div>
+        <div className={styles.logo}>
             <h1>PRIME NEWS</h1>
             {/* <p>Stay updated with the latest news</p> */}
             <p>English</p>
             <p>Malayalam</p>
-            <p>Bangla</p>
-            <p></p>
+            <p>Bengali</p>
+            <p>Hindi</p>
+            <p>Telugu</p>
+            <p>Marathi</p>
+            <p>Gujarati</p>
+            <p>Tamil</p>
+            <p>Kannada</p>
+            <p>Punjabi</p>
+            <p>Urdu</p>
+            <p>Oriya</p>
+            <p>Assamese</p>
         </div>
-        <div>
-
+        <div className={styles.searchBar}>
+          <h1>PRIME NEWS</h1>
+          <input type="text" placeholder='Enter your search' />
+          <button>Categories</button>
         </div>
     </div>
   )
