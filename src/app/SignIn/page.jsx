8d4e76d51ebc249cb4c308819e01cdcd4e1199fa
soumya tiwari <soumyatiwari7866@gmail.com>
2025-09-12 16:27:05 +0530

    Updated sign in page

--- src/app/SignIn/page.jsx
+++ src/app/SignIn/page.jsx
@@ -5,24 +5,68 @@ import styles from '@/Styles/SignIn/signin.module.scss'
 import Link from 'next/link'
 
 const SignIn = () => {
-    const [InputValue, setInputValue] = useState()
+    const [InputValue, setInputValue] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleSubmit = () => {
+        if (InputValue === "" || InputValue === null) {
+            alert("Please enter your email or username");
+            return;
+        }
+        if (password !== "" && password !== null) {
+            if (password.length > 8) {
+                if (password !== "@" || password !== "#" || password !== "$" || password !== "%" || password !== "&" || password !== "*") {
+                    alert("Password must contain at least one special character");
+                    return;
+                }
+                else if(password=== password.toLowerCase()){
+                    alert("Password must contain at least one uppercase letter");
+                    return;
+                }
+                else if(password== password.toUpperCase()){
+                    alert("Password must contain at least one lowercase letter");
+                    return;
+                }
+                else if(password=="1234567890"||password=="password"||password=="qwerty"||password=="admin"){
+                    alert("Password is too common, please choose a stronger password");
+                    return;
+                }
+            }
+            else{
+                alert("Password must be at least 8 characters long");
+            }
+        }
+        
+        else{
+            alert("Please enter your password");
+            return;
+        }
+        // console.log("button clicked");
+
+    }
+
     return (
         <div className={styles.signInContainer}>
+            <Link href="/">
+                <button className={styles.backBtn}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path fill="black" d="M73.4 297.4C60.9 309.9 60.9 330.2 73.4 342.7L233.4 502.7C245.9 515.2 266.2 515.2 278.7 502.7C291.2 490.2 291.2 469.9 278.7 457.4L173.3 352L544 352C561.7 352 576 337.7 576 320C576 302.3 561.7 288 544 288L173.3 288L278.7 182.6C291.2 170.1 291.2 149.8 278.7 137.3C266.2 124.8 245.9 124.8 233.4 137.3L73.4 297.3z" /></svg>
+                </button>
+            </Link>
             <div className={styles.signInHeader}>
                 <p className={styles.heading}>Log in</p>
                 <p className={styles.para}>Sign in to access your Account</p>
             </div>
             <div className={styles.inputContainer1}>
-                <label>Your username or email</label>
+                <label >Your username or email</label>
                 <div className={styles.inputBox1}>
-                    <input type="text" placeholder='user@example.com' />
+                    <input type="text" placeholder='user@example.com' value={InputValue} onChange={(e) => setInputValue(e.target.value)} />
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M320 312C386.3 312 440 258.3 440 192C440 125.7 386.3 72 320 72C253.7 72 200 125.7 200 192C200 258.3 253.7 312 320 312zM290.3 368C191.8 368 112 447.8 112 546.3C112 562.7 125.3 576 141.7 576L498.3 576C514.7 576 528 562.7 528 546.3C528 447.8 448.2 368 349.7 368L290.3 368z" /></svg>
                 </div>
             </div>
             <div className={styles.inputContainer2}>
                 <label>Password</label>
                 <div className={styles.inputBox2}>
-                    <input type="text" placeholder='************' />
+                    <input type="text" placeholder='************' onClick={(e) => setPassword(e.target.value)} />
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path d="M256 160L256 224L384 224L384 160C384 124.7 355.3 96 320 96C284.7 96 256 124.7 256 160zM192 224L192 160C192 89.3 249.3 32 320 32C390.7 32 448 89.3 448 160L448 224C483.3 224 512 252.7 512 288L512 512C512 547.3 483.3 576 448 576L192 576C156.7 576 128 547.3 128 512L128 288C128 252.7 156.7 224 192 224z" /></svg>
                 </div>
             </div>
@@ -33,9 +77,10 @@ const SignIn = () => {
                 </div>
                 <p>Forgot your password?</p>
             </div>
-            <Link href="/"><button className={styles.signInBtn}>
-                Sign in
-            </button>
+            <Link href="" >
+                <button onClick={handleSubmit} className={styles.signInBtn}>
+                    Sign in
+                </button>
             </Link>
             <div className={styles.signInLine}>
                 <hr />
@@ -63,9 +108,3 @@ const SignIn = () => {
 }
 
 export default SignIn;
-
-{/* <Link href="/">
-                    <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640"><path fill="#008040" d="M73.4 297.4C60.9 309.9 60.9 330.2 73.4 342.7L233.4 502.7C245.9 515.2 266.2 515.2 278.7 502.7C291.2 490.2 291.2 469.9 278.7 457.4L173.3 352L544 352C561.7 352 576 337.7 576 320C576 302.3 561.7 288 544 288L173.3 288L278.7 182.6C291.2 170.1 291.2 149.8 278.7 137.3C266.2 124.8 245.9 124.8 233.4 137.3L73.4 297.3z"/></svg>
-                    </button>
-                    </Link> */}
