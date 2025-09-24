dd2f907d91b26135f9fa008adfc25c7131d0888f
soumya tiwari <soumyatiwari7866@gmail.com>
2025-09-24 21:23:54 +0530

    created mainSection for mobile screen

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,6 +6,11 @@ import Link from 'next/link';
 import { MainSection } from '../MainSection';
 
 export const Header = ({ searchItem, setSearchItem, onSelect, setOnSelect }) => {
+  const [isIconClicked,setIsIconClicked] = useState(false);
+
+  const handleIconClick=()=>{
+    setIsIconClicked(true);
+  }
 
   return (
     <div className={styles.mainContainer}>
@@ -49,7 +54,7 @@ export const Header = ({ searchItem, setSearchItem, onSelect, setOnSelect }) => 
         </Link>
       </div> */}
       <div className={styles.searchBar}>
-        <svg xmlns="http://www.w3.org/2000/svg" className={styles.hamburgerIcon} viewBox="0 0 640 640"><path fill="#000000" d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" onClick={handleIconClick} className={styles.hamburgerIcon} viewBox="0 0 640 640"><path fill="#000000" d="M96 160C96 142.3 110.3 128 128 128L512 128C529.7 128 544 142.3 544 160C544 177.7 529.7 192 512 192L128 192C110.3 192 96 177.7 96 160zM96 320C96 302.3 110.3 288 128 288L512 288C529.7 288 544 302.3 544 320C544 337.7 529.7 352 512 352L128 352C110.3 352 96 337.7 96 320zM544 480C544 497.7 529.7 512 512 512L128 512C110.3 512 96 497.7 96 480C96 462.3 110.3 448 128 448L512 448C529.7 448 544 462.3 544 480z" /></svg>
         <h1>PRIME NEWS</h1>
         <div className={styles.searchInput}>
           <div className={styles.searchIcon}>
@@ -74,7 +79,14 @@ export const Header = ({ searchItem, setSearchItem, onSelect, setOnSelect }) => 
             <option value="Environment">Environment</option>
           </select>
         </div>
+        
       </div>
+      {isIconClicked?(
+          <>
+          <div className={styles.sidebar}>
+            <p>Hello</p>
+            </div></>
+        ):null}
     </div>
   )
 }
